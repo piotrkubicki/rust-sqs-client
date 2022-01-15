b916d1c4680ff7efb1514b8d7e01c7f7fbe6e4bd
Piotr Kubicki <pkubicki44@gmail.com>
2022-01-15 21:12:10 +0000

    Refactor code for testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use aws_sdk_sqs::client::fluent_builders;
-use aws_sdk_sqs::model::MessageAttributeValue;
-use aws_sdk_sqs::{Client, output::SendMessageOutput, output::GetQueueUrlOutput};
+use aws_sdk_sqs::{
+    Client,
+    output::GetQueueUrlOutput,
+};
 use std::{thread, time::Duration};
 use std::error::Error;
 
@@ -11,25 +13,61 @@ struct SqsHandler {
 }
 
 impl SqsHandler {
-    fn new(client: Client) -> SqsHandler {
-        SqsHandler { client }
+    fn new(client: Client) -> Self {
+        Self { client }
     }
+}
+
+struct GetQueueUrlResult {
+    name: String,
+    url: Option<String>,
+}
+
+struct SendMessageResult {
+    response: Option<String>,
 }
 
 #[async_trait::async_trait]
 pub trait MessageHandler {
-    fn get_queue_url(&self) -> fluent_builders::GetQueueUrl;
-    fn send_message(&self) -> fluent_builders::SendMessage;
+    async fn get_queue_url(&self, queue_name: &str) -> Result<GetQueueUrlResult, Box<dyn Error + Send + Sync + 'static>>;
+    async fn send_message(&self, queue_url: &str, message: &str, group_id: &str) -> Result<SendMessageResult, Box<dyn Error + Send + Sync + 'static>>;
 }
 
 #[async_trait::async_trait]
 impl MessageHandler for SqsHandler {
-    fn get_queue_url(&self) -> fluent_builders::GetQueueUrl {
-        self.client.get_queue_url()
+    async fn get_queue_url(&self, queue_name: &str) -> Result<GetQueueUrlResult, Box<dyn Error + Send + Sync + 'static>> {
+        let get_queue_url = self.client.get_queue_url();
+        let GetQueueUrlOutput { queue_url, .. } = get_queue_url.queue_name(queue_name).send().await?;
+        Ok(
+            GetQueueUrlResult {
+                name: queue_name.to_string(),
+                url: queue_url,
+            }
+        )
     }
 
-    fn send_message(&self) -> fluent_builders::SendMessage {
+    async fn send_message(&self, queue_url: &str, message: &str, group_id: &str) -> Result<SendMessageResult, Box<dyn Error + Send + Sync + 'static>> {
         self.client.send_message()
+            .queue_url(queue_url)
+            .message_body(message)
+            .message_group_id(group_id)
+            .send()
+            .await?;
+
+        Ok(
+            SendMessageResult {
+               response: Some("Message sent successfully!".to_string())
+            }
+        )
+    }
+}
+
+async fn get_queue_url(client: &dyn MessageHandler, queue_name: &str) -> Result<String, Box<dyn Error + Send + Sync + 'static>> {
+    let GetQueueUrlResult{ name, url } = client.get_queue_url(queue_name).await?;
+    if let Some(url) = url {
+        Ok(url)
+    } else {
+        Ok("".to_string())
     }
 }
 
@@ -37,10 +75,9 @@ impl MessageHandler for SqsHandler {
 async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
-    let message_handler = SqsHandler::new(client);
+    let message_handler: MessageHandler = SqsHandler::new(client);
 
-    let queue_url = get_queue_url(&message_handler).await?;
-    let GetQueueUrlOutput{ queue_url, .. } = queue_url;
+    let queue_url = message_handler.get_queue_url().await?;
     if let Some(queue_url) = queue_url {
         for i in 0..10 {
             let message = format!("Message NR {}", i);
@@ -56,17 +93,23 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     Ok(())
 }
 
-async fn get_queue_url(message_handler: &dyn MessageHandler) -> Result<GetQueueUrlOutput, Box<dyn Error + Send + Sync + 'static>> {
-    let get_queue_url = message_handler.get_queue_url();
-    let queue_url = get_queue_url.queue_name(QUEUE_NAME).send().await?;
-    Ok(queue_url)
-}
 
-async fn send_message(message_handler: &dyn MessageHandler, queue_url: &str, message: &str) -> Option<SendMessageOutput> {
-    message_handler.send_message()
-        .queue_url(queue_url)
-        .message_body(message)
-        .message_group_id("1")
-        .send()
-        .await.ok()
+#[cfg(test)]
+mod test {
+    use aws_sdk_sqs::operation::GetQueueUrl;
+
+    use super::*;
+
+    struct MockSqsClient {
+        queue_name: String,
+    }
+
+    #[async_trait::async_trait]
+    impl MessageHandler for MockSqsClient {
+        fn get_queue_url(&self) -> fluent_builders::GetQueueUrl {
+        }
+
+        fn send_message(&self) -> fluent_builders::SendMessage {
+        }
+    }
 }
